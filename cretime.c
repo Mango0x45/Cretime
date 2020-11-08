315f0849ae6cdd130109a1d94cf3c06119ce3f89
Mango0x45 <thomasvoss@live.com>
2020-11-08 01:45:01 +0100

    Fixed license in --help

--- cretime.c
+++ cretime.c
@@ -37,7 +37,7 @@ Report bugs to: thomasvoss@live.com \n\
 Source code: <https://www.github.com/Mango0x45/retime>\n"
 #define VERSION "\
 cretime v1.3 \n\
-Licence MIT: <https://mit-license.org/> \n\
+Licence BSD-3-Clause License: <https://opensource.org/licenses/BSD-3-Clause> \n\
 This is free software: you are free to change and redistribute it. \n\
 There is NO WARRANTY, to the extent permitted by law. \n\
 \n\
